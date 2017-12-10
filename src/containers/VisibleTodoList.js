cb545b657b823d1a963d661065442884a1728c3b
Cristian E. Avalos <cavalos0086@gmail.com>
2017-12-10 00:19:30 -0500

    finish tutorial

---
+++ src/containers/VisibleTodoList.js
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux';
+import { toggleTodo } from '../actions';
+import TodoList from '../components/TodoList';
+import constants from '../constants';
+
+const getVisibleTodos = (todos, filter) => {
+    switch(filter) {
+        case constants.SHOW_COMPLETED:
+            return todos.filter(t => t.completed)
+        case constants.SHOW_ACTIVE:
+            return todos.filter(t => !t.completed)
+        case constants.SHOW_ALL:
+        default:
+            return todos
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTodoClick: id => {
+            dispatch(toggleTodo(id))
+        }
+    }
+}
+
+const VisibleTodoList = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(TodoList)
+
+export default VisibleTodoList
