39cb337480cd635c644b8a4a08a00733626f7076
Cristian E. Avalos <cavalos0086@gmail.com>
2017-12-09 22:27:28 -0500

    adds actions to the app

---
+++ src/actions/index.js
@@ -0,0 +1,24 @@
+import constants from '../constants';
+
+let nextTodoId = 0;
+export const addTodo = text => {
+    return {
+        type: constants.ADD_TODO,
+        id: nextTodoId,
+        text
+    }
+}
+
+export const toggleTodo = id => {
+    return {
+        type: constants.TOGGLE_TODO,
+        id
+    }
+}
+
+export const setVisibilityFilter = filter => {
+    return {
+        type: constants.setVisibilityFilter,
+        filter
+    }
+}
